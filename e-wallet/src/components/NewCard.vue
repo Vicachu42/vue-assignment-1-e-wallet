ab7ec6cfe4874305f3c80305bb1ceda16995cf10
Vic <victoria.b.graf@gmail.com>
2021-03-12 13:58:15 +0100

    Backup

---
+++ e-wallet/src/components/NewCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1>Testing</h1>
+        <form>
+            <label>Card number</label>
+            <input type="text" />
+            <label>Cardholder name</label>
+            <input type="text" />
+            <label>Valid thru</label>
+            <input type="text" />
+            <label>CCV</label>
+            <input type="text" />
+            <label>card vendor</label>
+            <input type="text">
+            <select name="vendors" id="vendors">
+                <option value="bitcoin">Bitcoin</option>
+                <option value="evil">Evil Corp</option>
+                <option value="ninja">Ninja</option>
+                <option value="blockchain">Block Chain</option>
+            </select>
+        </form>
+        <!-- <form class="inputForm">
+            <label>Card number</label>
+            <input type="text" v-model="newCard.cardNumber" />
+            <label>Cardholder name</label>
+            <input type="text" v-model="newCard.cardHolder" />
+            <label>Valid thru</label>
+            <input type="text" v-model="newCard.valid" />
+            <label>CCV</label>
+            <input type="text" v-model="newCard.CCV" />
+            <label>Card vendor</label>
+            <input type="text" v-model="newCard.vendor" />
+            <select id="vendors">
+                <option value="bitcoin">Bitcoin</option>
+                <option value="evil">Evil Corp</option>
+                <option value="ninja">Ninja</option>
+                <option value="blockchain">Block Chain</option>
+            </select>
+        </form> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewCard'
+}
+</script>
+
+<style>
+
+</style>
--- e-wallet/src/views/AddCard.vue
+++ e-wallet/src/views/AddCard.vue
@@ -1,54 +1,30 @@
 <template>
   <div class="addCard">
     <Top message="Add new card" />
-    <section class="blankCard">
+
+    <NewCard />
+
+    <!-- <section class="blankCard">
     <Card>
-      <!-- <p>{{ add.number }}</p> -->
+      <p>{{ add.number }}</p>
     </Card>
-    </section>
-    <form class="inputForm">
-      <label>Card number</label>
-      <input type="text" v-model="add.number" />
-      <label>Cardholder name</label>
-      <input type="text" v-model="add.name" />
-      <label>Valid thru</label>
-      <input type="text" v-model="add.valid" />
-      <label>CCV</label>
-      <input type="text" v-model="add.CCV" />
-      <label>Card vendor</label>
-      <input type="text" v-model="add.vendor" />
-      <select id="vendors">
-        <option value="bitcoin">Bitcoin</option>
-        <option value="evil">Evil Corp</option>
-        <option value="ninja">Ninja</option>
-        <option value="blockchain">Block Chain</option>
-      </select>
-    </form>
+    </section> -->
+
   </div>
 </template>
 
 <script>
 import Top from '../components/Top.vue'
-import Card from '../components/Card.vue'
+// import Card from '../components/Card.vue'
+import NewCard from '../components/NewCard'
 
 export default {
   components: {
     Top,
-    Card
-  },
-  props: {
-    card: Object
-  },
-  data() {
-    return {
-      add: {
-        number: ''
-      }
-    }
+    NewCard
   }
 }
 </script>
 
 <style>
-
 </style>
